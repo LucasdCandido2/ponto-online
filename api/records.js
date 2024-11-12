fab91f77ecee7e06acb1405b1b915069677fcc02
LucasdCandido2 <lucasdcandido1994@gmail.com>
2024-11-12 16:57:10 -0300

    correções para funcionar na vercel

--- api/records.js
+++ api/records.js
@@ -26,21 +26,28 @@ module.exports = async (req, res) => {
     const recordId = req.url.split('/').pop(); // Captura o último segmento da URL como ID
     const updatedRecord = req.body;
   
+    console.log('Atualizando o registro com ID:', recordId);
+    console.log('Dados recebidos para atualização:', updatedRecord);
+  
     try {
       const data = await fs.readFile(recordsPath, 'utf8');
       const records = JSON.parse(data);
       const index = records.findIndex(record => record.id == recordId);
   
       if (index !== -1) {
+        console.log('Registro encontrado, atualizando...');
         records[index] = { ...records[index], ...updatedRecord };
         await fs.writeFile(recordsPath, JSON.stringify(records, null, 2));
         res.status(200).json({ message: 'Registro atualizado com sucesso' });
       } else {
+        console.log('Registro não encontrado');
         res.status(404).json({ message: 'Registro não encontrado' });
       }
     } catch (err) {
-      res.status(500).json({ message: 'Erro ao atualizar o registro' });
+      console.error('Erro ao atualizar o registro:', err);
+      res.status(500).json({ message: 'Erro ao atualizar o registro', error: err.message });
     }
+  
   } else {
     res.status(405).json({ message: 'Método não permitido' });
   }
